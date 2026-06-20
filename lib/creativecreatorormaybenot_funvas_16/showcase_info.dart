e2be8b7bb6770ad5081aebeb6f7c7c13eeb57498
Thamer Hussain <e_thahussain@earthlink.iq>
2026-06-20 13:22:56 +0300

    feat: remove all funvas showcases
    Delete the 50 creativecreatorormaybenot_funvas showcases and the
    funvas_shared package. Drop now-unused deps (funvas, open_simplex_2,
    google_fonts); keep shared_preferences for the particle clock.

--- lib/creativecreatorormaybenot_funvas_16/showcase_info.dart
+++
@@ -1,6 +0,0 @@
-import 'package:showcase_library/showcase_contract.dart';
-
-const showcaseInfo = ShowcaseInfo(
-  showcaseName: 'Funvas 16',
-  githubRepoUrl: 'https://github.com/creativecreatorormaybenot/funvas',
-);
--- lib/creativecreatorormaybenot_funvas_24/showcase_info.dart
+++
@@ -1,6 +0,0 @@
-import 'package:showcase_library/showcase_contract.dart';
-
-const showcaseInfo = ShowcaseInfo(
-  showcaseName: 'Funvas 24',
-  githubRepoUrl: 'https://github.com/creativecreatorormaybenot/funvas',
-);
